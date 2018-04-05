84a93e8927f540d139423f16e5d35a3c2d83d72c
Georgy Karpov <SunstriderEx@users.noreply.github.com>
2018-04-05 23:47:02 +0500

    Files upload

---
+++ Подсветка растений. Настройка/DataReader.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Подсветка_растений.Настройка
+{
+    public static class DataReader
+    {
+        public static string DataToTime(byte[] data)
+        {
+            string time = "Ошибка";
+            
+            if (data.Length > 0)
+            {
+                time = RtcCounterToTime(BitConverter.ToUInt32(data, 0));
+
+                /*var dataString = System.Text.Encoding.UTF8.GetString(data);
+                var timeParts = dataString.Split(':');
+                if (timeParts.Length == 3) // Часы, минуты и секунды
+                {
+                    string[] formattedTimeParts = new string[3];
+                    for (int i = 0; i < formattedTimeParts.Length; i++)
+                        formattedTimeParts[i] = timeParts[i].Length == 1 ? "0" + timeParts[i] : timeParts[i];
+
+                    time = string.Format("{0}:{1}:{2}", formattedTimeParts[0], formattedTimeParts[1], formattedTimeParts[2]);
+                }*/
+            }
+
+            return time;
+        }
+
+        private static string RtcCounterToTime(uint rtcCounter)
+        {
+            uint time;
+            uint t1;
+            string hour;
+            string min;
+            string sec;
+
+            time = rtcCounter;
+            t1 = time / 60;
+            sec = (time - t1 * 60).ToString();
+            sec = sec.Length == 1 ? "0" + sec : sec;
+
+            time = t1;
+            t1 = time / 60;
+            min = (time - t1 * 60).ToString();
+            min = min.Length == 1 ? "0" + min : min;
+
+            time = t1;
+            t1 = time / 24;
+            hour = (time - t1 * 24).ToString();
+            hour = hour.Length == 1 ? "0" + hour : hour;
+
+            return string.Format("{0}:{1}:{2}", hour, min, sec);
+        }
+
+        private static byte[] TimeToData(DateTime time)
+        {
+            return BitConverter.GetBytes(TimeToRtcCounter(time));
+        }
+
+        private static int TimeToRtcCounter(DateTime time)
+        {
+            int JDN = 0;
+
+            JDN += (time.Hour * 3600);
+            JDN += (time.Minute * 60);
+            JDN += (time.Second);
+
+            return JDN;
+        }
+
+        public static byte[] TimeToOutputData(DateTime time, ReportID timeType)
+        {
+            byte[] timeData = TimeToData(time);
+            byte[] data = new byte[timeData.Length + 2];
+            data[0] = 0; // первый байт теряется почему-то
+            data[1] = (byte) timeType;
+            for (int i = 0; i < timeData.Length; i++)
+                data[i + 2] = timeData[i];
+            return data;
+        }
+    }
+}
---
+++ Подсветка растений. Настройка/MainWindow.xaml.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using HidLibrary;
+
+namespace Подсветка_растений.Настройка
+{
+    public enum ReportID
+    {
+        EnableDeviceUSB,
+        DeviceRtcTime,
+        DeviceTimeToSleep,
+        DeviceTimeToAwake,
+        SetRtcTime,
+        SetTimeToSleep,
+        SetTimeToAwake
+    }
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static MainWindow instance;
+
+        private const int VendorId = 0x029A;
+        private const int ProductId = 0x0065;
+        private static HidDevice device;
+
+        private bool timeToSleepTextBoxChanged = false;
+        private bool timeToAwakeTextBoxChanged = false;
+        private Brush defaultBackground;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            instance = this;
+
+            defaultBackground = timeToSleepTextBox.Background;
+            sendSettingsButton.IsEnabled = false;
+            syncTimeButton.IsEnabled = false;
+            rtcTimeTextBox.IsEnabled = false;
+            timeToSleepTextBox.IsEnabled = false;
+            timeToAwakeTextBox.IsEnabled = false;
+            WaitForConnection();
+        }
+
+        private static void OnReport(HidReport report)
+        {
+            if (!device.IsConnected)
+                return;
+
+            device.ReadReport(OnReport);
+
+            var reportId = report.Data[0];
+
+            var data = new byte[report.Data.Length - 1];
+            for (int i = 0; i < report.Data.Length - 1; i++)
+                data[i] = report.Data[i + 1];
+
+            string time = DataReader.DataToTime(data);
+            
+            switch ((ReportID)reportId)
+            {
+                case ReportID.DeviceRtcTime:
+                    instance.Dispatcher.BeginInvoke(
+                        (Action)(() => instance.rtcTimeTextBox.Text = time));
+                    break;
+
+                case ReportID.DeviceTimeToSleep:
+                    if (!instance.timeToSleepTextBoxChanged)
+                        instance.Dispatcher.BeginInvoke(
+                            (Action)(() => instance.timeToSleepTextBox.Text = time));
+                    break;
+
+                case ReportID.DeviceTimeToAwake:
+                    if (!instance.timeToAwakeTextBoxChanged)
+                        instance.Dispatcher.BeginInvoke(
+                            (Action)(() => instance.timeToAwakeTextBox.Text = time));
+                    break;
+            }
+        }
+
+        private static void DeviceAttachedHandler()
+        {
+            device.ReadReport(OnReport);
+
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.statusBarLabel.Content = "Устройство подключено"));
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.sendSettingsButton.IsEnabled = true));
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.syncTimeButton.IsEnabled = true));
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.rtcTimeTextBox.IsEnabled = true));
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.timeToSleepTextBox.IsEnabled = true));
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.timeToAwakeTextBox.IsEnabled = true));
+        }
+
+        private static void DeviceRemovedHandler()
+        {
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.statusBarLabel.Content = "Устройство отключено"));
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.sendSettingsButton.IsEnabled = false));
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.syncTimeButton.IsEnabled = false));
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.rtcTimeTextBox.IsEnabled = false));
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.timeToSleepTextBox.IsEnabled = false));
+            instance.Dispatcher.BeginInvoke((Action)(() => instance.timeToAwakeTextBox.IsEnabled = false));
+        }
+
+        private async void WaitForConnection()
+        {
+            await WaitForConnectionTask();
+        }
+
+        private async Task WaitForConnectionTask()
+        {
+            while (device == null)
+            {
+                device = HidDevices.Enumerate(VendorId, ProductId).FirstOrDefault();
+
+                if (device != null)
+                {
+                    device.OpenDevice();
+
+                    device.Inserted += DeviceAttachedHandler;
+                    device.Removed += DeviceRemovedHandler;
+
+                    device.MonitorDeviceEvents = true;
+
+                    device.ReadReport(OnReport);
+
+                    statusBarLabel.Content = "Устройство обнаружено";
+
+                    device.CloseDevice();
+                }
+                else
+                {
+                    statusBarLabel.Content = "Ожидание подключения устройства";
+                    await Task.Delay(250);
+                }
+            }
+            
+        }
+
+        private void syncTimeButton_Click(object sender, RoutedEventArgs e)
+        {
+            syncTimeButton.IsEnabled = false;
+            device.WriteFeatureData(DataReader.TimeToOutputData(DateTime.Now, ReportID.SetRtcTime));
+            statusBarLabel.Content = string.Format("[{0}] Время синхронизировано", DateTime.Now.ToLongTimeString());
+            syncTimeButton.IsEnabled = true;
+        }
+
+        private void sendSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            sendSettingsButton.IsEnabled = false;
+
+            var timeToSleepStrs = timeToSleepTextBox.Text.Split(':');
+            var timeToAwakeStrs = timeToAwakeTextBox.Text.Split(':');
+            if (timeToSleepStrs.Length != 3 || timeToAwakeStrs.Length != 3)
+            {
+                MessageBox.Show("Время должно иметь формат ЧЧ:ММ:СС", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                sendSettingsButton.IsEnabled = true;
+                return;
+            }
+
+            DateTime timeToSleep, timeToAwake;
+
+            try
+            {
+                timeToSleep = new DateTime(2017, 09, 14, int.Parse(timeToSleepStrs[0]), int.Parse(timeToSleepStrs[1]), int.Parse(timeToSleepStrs[2]));
+                timeToAwake = new DateTime(2017, 09, 14, int.Parse(timeToAwakeStrs[0]), int.Parse(timeToAwakeStrs[1]), int.Parse(timeToAwakeStrs[2]));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверное значение часов, минут или секунд", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                sendSettingsButton.IsEnabled = true;
+                return;
+            }
+            
+            device.WriteFeatureData(DataReader.TimeToOutputData(timeToSleep, ReportID.SetTimeToSleep));
+            device.WriteFeatureData(DataReader.TimeToOutputData(timeToAwake, ReportID.SetTimeToAwake));
+
+            timeToSleepTextBoxChanged = false;
+            timeToAwakeTextBoxChanged = false;
+            timeToSleepTextBox.Background = defaultBackground;
+            timeToAwakeTextBox.Background = defaultBackground;
+            sendSettingsButton.IsEnabled = true;
+            statusBarLabel.Content = string.Format("[{0}] Настройки сохранены", DateTime.Now.ToLongTimeString());
+            System.Media.SystemSounds.Exclamation.Play();
+        }
+
+        private void timeToSleepTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            timeToSleepTextBoxChanged = true;
+            timeToSleepTextBox.Background = Brushes.Yellow;
+        }
+
+        private void timeToAwakeTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            timeToAwakeTextBoxChanged = true;
+            timeToAwakeTextBox.Background = Brushes.Yellow;
+        }
+    }
+}
